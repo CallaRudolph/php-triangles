4dc0854b44154db833fb1848933c5a5971896f7f
Calla Rudolph and Rena Rallis <calla@>
2017-06-26 15:23:51 -0700

    create Triangle class with three functions for equilateral, isosceles, and scalene

---
+++ app/app.php
@@ -0,0 +1,59 @@
+<?php
+date_default_timezone_set('America/Los_Angeles');
+
+require_once __DIR__."/../vendor/autoload.php";
+require_once __DIR__."/../src/triangles.php";
+
+$app = new Silex\Application();
+
+$app->get("/", function() {
+    return "Home";
+});
+$app->get("/new_triangles", function() {
+        return "
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
+            <title>Make a triangle!</title>
+        </head>
+        <body>
+            <div class='container'>
+                <h1>Triangle Maker</h1>
+                <p>Enter the dimensions of your triangle to see what kind it is.</p>
+                <form action='/view_triangle'>
+                    <div class='form-group'>
+                      <label for='length1'>Enter one side:</label>
+                      <input id='length1' name='length1' class='form-control' type='number'>
+                    </div>
+                    <div class='form-group'>
+                      <label for='length2'>Enter second side:</label>
+                      <input id='length2' name='length2' class='form-control' type='number'>
+                    </div>
+                    <div class='form-group'>
+                      <label for='length3'>Enter third side:</label>
+                      <input id='length3' name='length3' class='form-control' type='number'>
+                    </div>
+                    <button type='submit' class='btn-success'>Create</button>
+                </form>
+            </div>
+        </body>
+        </html>
+        ";
+});
+$app->get("/view_triangle", function() {
+  $my_triangle = new Triangle($_GET['length1'], $_GET['length2'], $_GET['length3']);
+  if ($my_triangle->isEquilateral()) {
+      return "<h1>You made an equilateral triangle!</h1>";
+  } else if ($my_triangle->isIsosceles()) {
+      return "<h1>You made an isosceles triangle!</h1>";
+  } else if ($my_triangle->isScalene()) {
+      return "<h1>You made a scalene triangle!</h1>";
+  } else {
+      return "you fail";
+  }
+});
+return $app;
+
+
+?>
---
+++ src/triangles.php
@@ -0,0 +1,77 @@
+<?php
+    class Triangle
+    {
+        private $length1;
+        private $length2;
+        private $length3;
+
+        function __construct($length1, $length2, $length3)
+        {
+            $this->length1 = $length1;
+            $this->length2 = $length2;
+            $this->length3 = $length3;
+        }
+
+        function isEquilateral()
+        {
+            if($this->length1 === $this->length2 && $this->length1 === $this->length3) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        function isIsosceles()
+        {
+            if($this->length1 === $this->length2 && $this->length1 != $this->length3) {
+                return true;
+            } else if ($this->length1 === $this->length3 && $this->length1 != $this->length2) {
+                return true;
+            } else if ($this->length2 === $this->length3 && $this->length2 != $this->length1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        function isScalene()
+        {
+            if($this->length1 != $this->length2 && $this->length2 != $this->length3 && $this->length1 != $this->length3) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        function setLength1($new_length1)
+        {
+            $this->length1 = (float) $new_length1;
+        }
+
+        function getLength1()
+        {
+            return $this->length1;
+        }
+
+        function setLength2($new_length2)
+        {
+            $this->length2 = (float) $new_length2;
+        }
+
+        function getLength2()
+        {
+            return $this->length2;
+        }
+
+        function setLength3($new_length3)
+        {
+            $this->length3 = (float) $new_length3;
+        }
+
+        function getLength3()
+        {
+            return $this->length3;
+        }
+
+    }
+?>
